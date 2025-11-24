92de3f7ba164c432e88bd6af11c5e5990ed8563a
banner2007 <47283735+banner2007@users.noreply.github.com>
2025-11-23 20:36:53 -0500

    Delete api directory

--- api/binance/time.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-    const response = await fetch("https://api.binance.com/api/v3/time");
-    const data = await response.json();
-    res.status(200).json(data);
-  }
-  
